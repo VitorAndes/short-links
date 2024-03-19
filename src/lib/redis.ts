e526a4cac03299dfdd5760597fbf042e37746d29
VitorAndes <vitor.andes.santos04@gmail.com>
2024-03-19 18:03:36 -0400

    teste

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,8 +1,7 @@
 import { createClient } from "redis";
 
 export const redis = createClient({
-  url: "redis://default:72c6259f429b4fc4bdade5ac7b97f648@ultimate-liger-46592.upstash.io:46592",
+  // secret
 });
 
 redis.connect();
-// secret
