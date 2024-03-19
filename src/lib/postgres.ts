9bd887168ba0e9128d9b80904fc4c6e26da83a75
VitorAndes <vitor.andes.santos04@gmail.com>
2024-03-19 17:27:37 -0400

    Enviar Links, Mostrar Links enviados, Redirecionamento dos links, Metricas dos links enviados

---
+++ src/lib/postgres.ts
@@ -0,0 +1,5 @@
+import postgres from "postgres";
+
+export const sql = postgres(
+  "postgresql://DB%20Postgres_owner:kT8Ude9ubayS@ep-empty-bread-a5iiu42c.us-east-2.aws.neon.tech/DBPostgres?sslmode=require"
+);
